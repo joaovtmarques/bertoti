50a57c2469386cc51fac53ae2928586a0b428963
João Vitor Marques <jvsilvam@outlook.com>
2022-09-26 19:13:40 -0300

    add estacionamento java project

---
+++ engenharia_de_software/projetos/estacionamento/src/Carro.java
@@ -0,0 +1,25 @@
+public class Carro {
+  private String placa;
+  private Especificacao espec;
+
+  public Carro(String placa, Especificacao espec) {
+    this.placa = placa;
+    this.espec = espec;
+  }
+
+  public String getPlaca() {
+    return placa;
+  }
+
+  public void setPlaca(String novaPlaca){
+    placa = novaPlaca;
+  }
+
+  public Especificacao getEspec() {
+    return espec;
+  }
+
+  public void setEspec(Especificacao espec) {
+    this.espec = espec;
+  }
+}

84efadf614d6dcd24de99e43cc60a54a67cdc51e
João Vitor Marques <jvsilvam@outlook.com>
2024-08-16 11:22:16 -0300

    get user input

--- padroes_de_projetos/strategy/taxa/src/App.java
+++ padroes_de_projetos/strategy/taxa/src/App.java
@@ -7,13 +7,18 @@ import models.Camiseta;
 public class App {
   public static void main(String[] args) throws Exception {
     
-    Blusa blusa = new Blusa("M", 0.7);
-    Camiseta camiseta = new Camiseta("M", 0.45);
+    System.out.print("Digite o peso da blusa: ");
+    Double pesoBlusa = Double.parseDouble(System.console().readLine());
+    System.out.print("Digite o peso da camiseta: ");
+    Double pesoCamiseta = Double.parseDouble(System.console().readLine());
+
+    Blusa blusa = new Blusa("M", pesoBlusa);
+    Camiseta camiseta = new Camiseta("M", pesoCamiseta);
 
     BigDecimal blusaTaxa = new BigDecimal(blusa.calculaTaxa());
     BigDecimal camisetaTaxa = new BigDecimal(camiseta.calculaTaxa());
 
-    NumberFormat nf = NumberFormat.getCurrencyInstance(); 
+    NumberFormat nf = NumberFormat.getCurrencyInstance();
 
     System.out.println("Valor da taxa de envio para a blusa no tamanho " + 
           blusa.getTamanho() + ": " + nf.format(blusaTaxa));
